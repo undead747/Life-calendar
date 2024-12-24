60c48c826bc31b795ad161b320a7f6a364c79d3c
unknown <minh.trancong@NSK.LOCAL>
2024-12-24 15:23:43 +0900

    fix size home page + add home icon

--- components/RedirectPagesList.tsx
+++ components/RedirectPagesList.tsx
@@ -1,4 +1,4 @@
-import { faPiggyBank, faClock } from '@fortawesome/free-solid-svg-icons'
+import { faPiggyBank, faClock, faHouse } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import TrelloIcon from './TrelloIcon'
@@ -7,6 +7,16 @@ import Image from 'next/image'
 export default function RedirectPagesList() {
     return (
         <div className='absolute top-[76px] left-0 flex justify-end items-center z-[1] bg-white w-full h-10 shadow-lg pl-2 pr-2'>
+            <a
+                href="/home"
+                className="flex items-center ml-8 text-black"
+                style={{borderBottom: '2px solid black'}}
+            >
+                <FontAwesomeIcon
+                    icon={faHouse}
+                />
+                <span className='text-xs ml-2 mt-1'>Home</span>
+            </a>
             <a
                 href="/home/pomodoro"
                 className="flex items-center ml-8 text-black no-underline"
